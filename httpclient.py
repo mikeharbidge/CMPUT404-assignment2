6480151d60a8d8472993d629a0706863f76a4905
Michael <mikeharbidge@gmail.com>
2023-10-13 15:23:04 -0600

    Get and Post mostly working, tests dont

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,8 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+import errno
+import time
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -38,6 +40,7 @@ class HTTPClient(object):
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
+        
         return None
 
     def get_code(self, data):
@@ -59,39 +62,55 @@ class HTTPClient(object):
     def recvall(self, sock):
         buffer = bytearray()
         done = False
+
+        start = time.time()
         while not done:
-            part = sock.recv(1024)
+            if (time.time() - start) > 2:
+                    done = True
+            part = sock.recv(4096)
             if (part):
-                print("RECV Part: "+part)
                 buffer.extend(part)
+                
             else:
                 done = not part
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
         parse = urllib.parse.urlparse(url)
-        print(parse)
-        #if first split: does not contain leading http://, prepend that
-
+        #print(parse)
+        #if split has no port, add default from https or http
         pathSplit = parse.netloc.split(":")
         address = pathSplit[0]
-        port = pathSplit[1]
+        if len(pathSplit) <= 1:
+            if parse.scheme.lower() == "https":
+                port = "443"
+            else:
+                port = "80"
+        else:
+            #port given
+            port = pathSplit[1]
 
-        print(address,port)
+        #print(address,port)
         
         self.connect(address,int(port))
 
-        request = "GET "+parse.path +"/ HTTP/1.1 \r\nHost: "+address+":"+port
-        print("Request:\n"+request)
+        request = "GET "+parse.path +" HTTP/1.1 \r\nHost: "+address+":"+port+"\r\n"
+        request += "Connection: Keep-Alive\r\n"
+        #print("Request:\n"+request)
 
         self.sendall(request)
 
         response = self.recvall(self.socket)
-        print("Response:\n"+response)
+        #print("Response:\n"+response)
 
         self.close()
 
-        code = 500
+
+        resList = response.split("\r\n")
+
+        self.close()
+
+        code = resList[0]
         body = ""
 
         
@@ -99,31 +118,53 @@ class HTTPClient(object):
 
     def POST(self, url, args=None):
         parse = urllib.parse.urlparse(url)
-        print(parse)
-        #if first split: does not contain leading http://, prepend that
-
+        #print(parse)
+        #if split has no port, add default from https or http
         pathSplit = parse.netloc.split(":")
         address = pathSplit[0]
-        port = pathSplit[1]
+        if len(pathSplit) <= 1:
+            if parse.scheme.lower() == "https":
+                port = "443"
+            else:
+                port = "80"
+        else:
+            #port given
+            port = pathSplit[1]
 
-        print(address,port)
+        #print(address,port)
         
         self.connect(address,int(port))
 
-        request = "POST "+parse.path +" HTTP/1.1 \r\nHost: "+address+":"+port
-        print("Request:\n"+request)
+        request = "POST "+parse.path +" HTTP/1.1 \r\nHost: "+address+":"+port+"\r\n"
+        request += "Content-Type: application/x-www-form-urlencoded\r\n"
+        #print("Request:\n"+request)
+
+        body = "\r\n"
+        length = 0
+        if (args):
+            for arg in args:
+                length += len(arg)
+                body += arg
+            request += "Content-Length: " + str(length)
+            request += body
 
         self.sendall(request)
 
         response = self.recvall(self.socket)
-        print("Response:\n"+response)
 
-        code = 500
+        resList = response.split("\r\n")
+
+        self.close()
+
+        code = resList[0]
         body = ""
+
+        #print(code,body)
+        
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
-        if (command == "POST"):
+        if (command.upper() == "POST"):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
@@ -136,5 +177,7 @@ if __name__ == "__main__":
         sys.exit(1)
     elif (len(sys.argv) == 3):
         print(client.command( sys.argv[2], sys.argv[1] ))
+    elif (len(sys.argv) == 4):
+        print(client.command( sys.argv[2], sys.argv[1], sys.argv[4] ))
     else:
         print(client.command( sys.argv[1] ))
